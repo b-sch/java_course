329d104bc5ba5e1524b3d85fdd17d0e5e4176fe6
bsch admin <bartlomiej.schneider@gmail.com>
2023-04-10 13:27:30 +0200

    new exercise added

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Harry";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "JavaMasterclass");
+
+            System.out.println(s);
+        }
+
+
     }
 }
