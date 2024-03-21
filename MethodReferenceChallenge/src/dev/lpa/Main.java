4d10691af5a1430861b942765556602b3c808b5a
b-sch <bartlomiej.schneider@gmail.com>
2024-03-21 14:27:15 +0100

    course updates

--- MethodReferenceChallenge/src/dev/lpa/Main.java
+++ MethodReferenceChallenge/src/dev/lpa/Main.java
@@ -3,38 +3,47 @@ package dev.lpa;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] names = {
-                "Anna",
-                "Bob",
-                "Mike",
-                "Kenny"
-        };
+        String[] names = {"Anna", "Bob", "Mike", "Stan", "Cartman"};
+        List<UnaryOperator<String>> listOfTransforms = new ArrayList<>(List.of(
+                String::toUpperCase,
+                s -> s.concat(getRandomCharacter()),
+                s -> s.concat(getReversedName(s)),
+                String::toLowerCase
+        ));
 
-        Supplier<Random> randomiser = Random::new;
-        Function<Random, String> characterGenerator = (r) -> Character.toString(r.nextInt(65, 90));
+        List<String> namesList = new ArrayList<>(List.of("Anna", "Bob", "Mike", "Stan", "Cartman"));
 
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            System.out.println((char) random.nextInt(65, 90));
-        }
-
-        UnaryOperator<String> addMiddleInitial = (s) -> s.concat(s);
-
-        List<UnaryOperator<String>> listOfTransformations = new ArrayList<>(
-                List.of(
-                        String::toUpperCase,
-                        (s) -> s.concat("s")
-                )
-        );
-
+        processNames(namesList, listOfTransforms);
+//        namesList.replaceAll(String::toUpperCase);
+//        namesList.replaceAll(s -> s.concat(getRandomCharacter()));
+//        namesList.replaceAll(s -> s.concat(getReversedName(s)));
         
     }
 
+    public static String getReversedName(String s) {
+        return " " + new StringBuilder(s.substring(0, s.length() - 3)).reverse();
+    }
+
+    public static String getRandomCharacter() {
+        return " %S.".formatted(Character.toString(new Random().nextInt(65, 90)));
+    }
+
+    public static void processNames(List<String> namesList, List<UnaryOperator<String>> transforms) {
+//        namesList.replaceAll(s -> {
+//            for (var transform : transforms) {
+//                s = transform.apply(s);
+//            }
+//            return s;
+//        });
+
+        for (var transform : transforms) {
+            namesList.replaceAll(s -> s.transform(transform));
+            System.out.println(namesList);
+        }
+    }
 }
