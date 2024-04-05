87d1bbb6e0d99f2cf932836619f1eceade35eecb
b-sch <bartlomiej.schneider@gmail.com>
2024-04-05 16:13:54 +0200

    new lesson

--- Hashing/src/Main.java
+++ Hashing/src/Main.java
@@ -1,5 +1,55 @@
+import dev.lpa.PlayingCard;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String aText = "Hello";
+        String bText = "Hello";
+        String cText = String.join("l", "He", "lo");
+        String dText = "He".concat("llo");
+        String eText = "hello";
+
+        List<String> hellos = Arrays.asList(aText, bText, cText, dText, eText);
+
+        hellos.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+
+        Set<String> mySet = new HashSet<>(hellos);
+        System.out.println("mySet: " + mySet);
+        System.out.println("# of elements: " + mySet.size());
+
+        for (String setValue : mySet) {
+            System.out.println(setValue + ": ");
+            for (int i = 0; i < hellos.size(); i++) {
+                if (setValue == hellos.get(i)) {
+                    System.out.print(i + ", ");
+                }
+            }
+            System.out.println(" ");
+        }
+
+        System.out.println("-".repeat(50));
+
+        PlayingCard aceHearts = new PlayingCard("Hearts", "Ace");
+        PlayingCard kingClubs = new PlayingCard("Clubs", "King");
+        PlayingCard queenSpades = new PlayingCard("Spades", "Queen");
+
+        List<PlayingCard> cards = Arrays.asList(aceHearts, kingClubs, queenSpades);
+        cards.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+
+        System.out.println("-".repeat(50));
+        
+        Set<PlayingCard> deck = new HashSet<>();
+        for (PlayingCard card : cards) {
+            if (!deck.add(card)) {
+                System.out.println("Deck contains duplicate card: " + card);
+            }
+        }
+        System.out.println(deck);
     }
 }
