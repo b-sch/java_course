87d064d12bc52557d5dedd141ccf05dadfdb9f41
b-sch <bartlomiej.schneider@gmail.com>
2024-04-08 12:30:39 +0200

    new lesson

--- SetOperationsChallengeCourseVersion/src/dev/lpa/Main.java
+++ SetOperationsChallengeCourseVersion/src/dev/lpa/Main.java
@@ -9,9 +9,40 @@ public class Main {
         sortAndPrint("All Tasks", tasks);
 
         Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
-        Set<Task> annstasks = TaskData.getData("Ann");
-        sortAndPrint("Ann's Tasks", annstasks, sortByPriority);
+        Set<Task> annsTasks = TaskData.getData("Ann");
+        sortAndPrint("Ann's Tasks", annsTasks, sortByPriority);
 
+        Set<Task> bobsTasks = TaskData.getData("Bob");
+        Set<Task> carolsTasks = TaskData.getData("Carol");
+        List<Set<Task>> sets = List.of(annsTasks, bobsTasks, carolsTasks);
+
+        Set<Task> assignedTasks = getUnion(sets);
+        sortAndPrint("Assgined Tasks", assignedTasks);
+
+        Set<Task> everyTask = getUnion(List.of(tasks, assignedTasks));
+        sortAndPrint("The True All Tasks", everyTask);
+
+        Set<Task> missingTasks = getDifference(everyTask, tasks);
+        sortAndPrint("Missing Tasks", missingTasks);
+
+        Set<Task> unassignedTasks = getDifference(tasks, assignedTasks);
+        sortAndPrint("UnassignedTasks Tasks", unassignedTasks, sortByPriority);
+
+        Set<Task> overlap = getUnion(List.of(
+                getIntersect(annsTasks, bobsTasks),
+                getIntersect(carolsTasks, bobsTasks),
+                getIntersect(annsTasks, carolsTasks)
+        ));
+        sortAndPrint("Assigned to Multiples", overlap, sortByPriority);
+
+        List<Task> overlapping = new ArrayList<>();
+        for (Set<Task> set : sets) {
+            Set<Task> dupes = getIntersect(set, overlap);
+            overlapping.addAll(dupes);
+        }
+
+        Comparator<Task> priorityNatural = sortByPriority.thenComparing(Comparator.naturalOrder());
+        sortAndPrint("Overlapping", overlapping, priorityNatural);
     }
 
     private static void sortAndPrint(String header, Collection<Task> collection) {
@@ -26,5 +57,29 @@ public class Main {
         List<Task> list = new ArrayList<>(collection);
         list.sort(sorter);
         list.forEach(System.out::println);
+    }
+
+    private static Set<Task> getUnion(List<Set<Task>> sets) {
+
+        Set<Task> union = new HashSet<>();
+        for (var taskSet : sets) {
+            union.addAll(taskSet);
+        }
+        return union;
+    }
+
+    private static Set<Task> getIntersect(Set<Task> a, Set<Task> b) {
+
+        Set<Task> intersect = new HashSet<>(a);
+        intersect.retainAll(b);
+
+        return intersect;
+    }
+
+    private static Set<Task> getDifference(Set<Task> a, Set<Task> b) {
+
+        Set<Task> result = new HashSet<>(a);
+        result.removeAll(b);
+        return result;
     }
 }
